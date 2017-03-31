0ed421a844cf9c6c827e06c115fd9dd0032e50ac
Fredrik Linde <gearfreak@hotmail.se>
2017-03-31 06:50:41 -0700

    Added converter base

---
+++ main.cpp
@@ -0,0 +1,38 @@
+
+// This is required to link DirectX from the Windows SDK
+
+#include <d3d11.h>	
+#include <d3dcompiler.h>	
+
+#pragma comment (lib, "d3d11.lib")
+#pragma comment (lib, "d3dcompiler.lib")
+
+// The core header file of the FBX SDK
+#include <fbxsdk.h>
+
+#include <iostream>
+#include <istream>
+#include <ostream>
+
+// In order to link FBX SDK, do the following steps:
+
+// 1. In Properties -> General, set Character Set to "Unicode" to use DirectX wstrings
+
+// 2. In Properties -> VC++ Directories, add "FBX SDK\include" to Include Directories
+
+// 3. In Properties -> C++ -> Preprocessor, add "FBXSDK_SHARED" to Preprocessor Definitions
+
+// 4. In Properties -> C++ -> Code Generation, set Runtime Library to "Multi-threaded Debug DLL (/MDd)" for Debug 
+// and "Multi-threaded DLL (/MD)" for Release
+
+// 5. In Properties -> Linker -> General, set Additional Library Directories to "FBX SDK\lib\vs2012\x64\release" (or debug depending on configuration)
+
+// 6. In Properties -> Linker -> Input, add "libfbxsdk.lib" to Additional Dependencies
+
+using namespace std;
+
+int main() {
+
+	return 0;
+}
+
