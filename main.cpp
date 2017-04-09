bb817b26e5fa7cf12dff8548d0edabfc1d7cadd3
Fredrik Linde <gearfreak@hotmail.se>
2017-04-09 14:32:37 +0200

    Re-enabled write to file function

--- main.cpp
+++ main.cpp
@@ -46,7 +46,7 @@ int main() {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);	// Memory leak detection flag
 
 	Converter.Load("FbxModel\\cubes.fbx");
-	//Converter.writeToFile();
+	Converter.writeToFile();
 	
 	getchar();
 	
