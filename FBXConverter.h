0fa7d78d24dbde896c40ab22fcfa1f864008c60b
Fredrik Linde <gearfreak@hotmail.se>
2017-04-20 10:50:00 +0200

    Cleaned up master

--- FBXConverter.h
+++ FBXConverter.h
@@ -23,52 +23,7 @@ class FBXConverter {
 
 public:
 
-	FBXConverter();
-	~FBXConverter();
 
-	void ReleaseAll();
-
-	bool Load(const char *fileName);
-
-	bool LoadFBXFormat(const char *fileName);
-
-	void LoadMeshes();
-	void ProcessControlPoints(Mesh &pMesh);
-	
-	void CheckSkeleton(Mesh &pMesh);
-
-	void CreateVertexData(Mesh &pMesh);
-
-	void LoadLights();
-	void LoadCameras();
-	void writeToFile();
-	void LoadMorphAnim(FbxScene* scene);
-
-	void LoadSkeletonHierarchy(Mesh &pMesh);
-
-	void RecursiveDepthFirstSearch(Mesh &pMesh, FbxNode* node, int depth, int index, int parentIndex);
-
-	void GatherAnimationData(Mesh &pMesh);
-
-
-	FbxAMatrix GetGeometryTransformation(FbxNode* node);
-	unsigned int FindJointIndexByName(std::string& jointName, Skeleton skeleton);
-
-	void ConvertToLeftHanded(FbxAMatrix &matrix);
-
-private:
-
-	FbxManager* gFbxSdkManager;
-	FbxIOSettings* pIOsettings;
-
-	FbxImporter* pImporter;
-	FbxScene* pFbxScene;
-
-	FbxNode* pFbxRootNode;
-
-	vector<Mesh> meshes;
-	vector<Light> lights;
-	vector<Camera> cameras;
 
 };
 
